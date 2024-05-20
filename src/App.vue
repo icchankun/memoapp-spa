0acc2c1b1cdb15d98d74bfa3edcc578ce4be890d
icchankun <ikkun.official@gmail.com>
2024-05-20 17:07:29 +0900

    メモアプリSPA版を作成する。

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,58 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from 'vue'
+import MemoForm from './components/MemoForm.vue'
+import MemoList from './components/MemoList.vue'
+
+const showedMemo = ref({})
+const isEditing = ref(false)
+
+const visibility = computed(() => ({
+  visibility: isEditing.value ? 'visible' : 'hidden'
+}))
+const fetchSavedId = () => {
+  const savedId = localStorage.getItem('id')
+  return savedId ? JSON.parse(savedId) : 0
+}
+const fetchSavedMemos = () => {
+  const savedMemos = localStorage.getItem('memos')
+  return savedMemos ? JSON.parse(savedMemos) : []
+}
+
+const id = ref(fetchSavedId())
+const memos = ref(fetchSavedMemos())
+
+const showMemo = (memo) => {
+  isEditing.value = true
+  showedMemo.value = memo
+}
+const addMemo = () => {
+  showMemo({ id: ++id.value, content: '新規メモ' })
+  memos.value.push(showedMemo.value)
+  localStorage.setItem('memos', JSON.stringify(memos.value))
+  localStorage.setItem('id', showedMemo.value.id)
+}
+const updateMemo = (memo, updatedContent) => {
+  isEditing.value = false
+  showedMemo.value = {}
+  const memoIndex = memos.value.indexOf(memo)
+  memos.value.splice(memoIndex, 1, { id: memo.id, content: updatedContent })
+  localStorage.setItem('memos', JSON.stringify(memos.value))
+}
+const deleteMemo = (showedMemoId) => {
+  isEditing.value = false
+  memos.value = memos.value.filter((memo) => memo.id !== showedMemoId)
+  localStorage.setItem('memos', JSON.stringify(memos.value))
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <MemoList :showedMemo="showedMemo" :memos="memos" @addMemo="addMemo" @showMemo="showMemo" />
+  <MemoForm
+    :style="visibility"
+    :showedMemo="showedMemo"
+    @updateMemo="updateMemo"
+    @deleteMemo="deleteMemo"
+  />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
